d2f6ca1840cb0bda6a9f0f95fc747ee7970c0a1f
FengHZ <fenghz@zju.edu.cn>
2023-06-26 17:48:09 +0800

    add math require

--- chat/views.py
+++ chat/views.py
@@ -495,6 +495,7 @@ def build_messages(
     system_prompt = """You are ChatGPT, a large language model trained by OpenAI.
     Knowledge cutoff: 2021-09
     Current date: {}
+    Return all the inline math symbols within single $ and standalone formula with in double $. For example, $\pi$.
     """.format(current_time)
     ordered_messages_list.append({'is_bot': False, 'message': new_message_content})
 
