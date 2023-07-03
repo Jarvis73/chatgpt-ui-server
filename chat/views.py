972ac3cc05c905d133d3c43f9aab3165aebaa1eb
FengHZ <fenghz@zju.edu.cn>
2023-07-04 00:10:34 +0800

    change to openai

--- chat/views.py
+++ chat/views.py
@@ -154,26 +154,26 @@ class MaskViewSet(viewsets.ModelViewSet):
 
 
 MODELS = {
-    'gpt-3.5-turbo': {
-        'name': 'gpt-3.5-turbo-0613',
-        'key_name': 'gpt-3.5-turbo-azure',
-        'max_tokens': 4096,
-        'max_prompt_tokens': 1596,
-        'max_response_tokens': 2500,
-        'azure': True,
-        'kwargs': {
-            'engine': '35-0613'
-        },
-    },
     # 'gpt-3.5-turbo': {
     #     'name': 'gpt-3.5-turbo-0613',
-    #     'key_name': 'gpt-3.5-turbo',
+    #     'key_name': 'gpt-3.5-turbo-azure',
     #     'max_tokens': 4096,
     #     'max_prompt_tokens': 1596,
     #     'max_response_tokens': 2500,
-    #     'azure': False,
-    #     'kwargs': {},
+    #     'azure': True,
+    #     'kwargs': {
+    #         'engine': '35-0613'
+    #     },
     # },
+    'gpt-3.5-turbo': {
+        'name': 'gpt-3.5-turbo-0613',
+        'key_name': 'gpt-3.5-turbo',
+        'max_tokens': 4096,
+        'max_prompt_tokens': 1596,
+        'max_response_tokens': 2500,
+        'azure': False,
+        'kwargs': {},
+    },
     'gpt-3.5-turbo-16k': {
         'name': 'gpt-3.5-turbo-16k-0613',
         'key_name': 'gpt-3.5-turbo',
