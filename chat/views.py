ee139a3d90bfabb9f6fc1e2a581b4918b5d39753
FengHZ <630153601@qq.com>
2023-10-22 18:55:23 +0800

    change model name

--- chat/views.py
+++ chat/views.py
@@ -192,7 +192,7 @@ MODELS = {
         #     },
         # },
         {
-            'name': 'gpt-3.5-turbo-16k',
+            'name': 'gpt-3.5-turbo-16k-0613',
             'key_name': 'gpt-3.5-turbo',
             'max_tokens': 16384,
             'max_prompt_tokens': 2384,
